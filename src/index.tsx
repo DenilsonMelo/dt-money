85ea16216aab66a4bc8558f054864843f06acfc8
DenilsonMelo <denilsonmlo42@gmail.com>
2022-04-28 22:37:03 -0300

    using MirageJs how fake api

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createServer } from 'miragejs'
 import { App } from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+createServer({
+  routes(){
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return[
+        {
+          id: 1,
+          title: 'Futebol',
+          amount: 10,
+          type: 'deposity',
+          category: 'lazer',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
+
 root.render(
   <React.StrictMode>
     <App />
