042a8e681ddccb832e8f61a714d54caa92de9de1
DenilsonMelo <denilsonmlo42@gmail.com>
2022-05-01 14:36:27 -0300

    added modal new transaction and config transaction in api

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 import { App } from './App';
 
 const root = ReactDOM.createRoot(
@@ -8,20 +8,44 @@ const root = ReactDOM.createRoot(
 );
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Desenvolvimento de site',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2022-01-05 14:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Moradia',
+          amount: 1200,
+          createdAt: new Date('2022-04-03 14:00:00'),
+        }
+      ]
+    })
+  },
+
   routes(){
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return[
-        {
-          id: 1,
-          title: 'Futebol',
-          amount: 10,
-          type: 'deposity',
-          category: 'lazer',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction');
+    });
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
     })
   }
 })
